c9722a068c82d8ac26572f36732592cf7fc94868
autobuscgt <lekiaaaskya@gmail.com>
2024-07-10 17:22:48 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from "./hooks/usefulComponents";
-
-
+import logo from "./images/title.png"
 function App() {
   const {onToggleButton, tg,onClose} = useTelegram();
 
@@ -11,7 +10,8 @@ function App() {
   },[tg])
   return (
       <div className="App">
-     <h1>Hello world</h1>
+     <h1>Elements</h1>
+     <img src={logo} alt='Test Screen'></img>
      <button onClick={onToggleButton}>Toggle</button>
      <button onClick={onClose}>Закрыть</button>
     </div>
