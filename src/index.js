9e18b7a03433e7b43fa60b5b84b31fab9630b842
autobuscgt <lekiaaaskya@gmail.com>
2024-07-11 17:32:43 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import TimeTable from './pages/TimeTable';
 import Homework from './pages/Homework';
+import News from './pages/News';
+import Main from './pages/Main';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
