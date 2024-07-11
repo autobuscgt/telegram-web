1d72ade6fc6350339634882384ed0130ac81c8ee
autobuscgt <lekiaaaskya@gmail.com>
2024-07-11 19:09:22 +0300

    changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import './styles/App.css';
-import './styles/Buttons.css';
+import '../styles/App.css';
+import '../styles/Buttons.css';
 function NavBar() {
     return (
         <div style={{marginTop:'2%'}}>
