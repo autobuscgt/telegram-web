b3bcff20ae2c1e02ad818eab37910f9ee027a711
autobuscgt <lekiaaaskya@gmail.com>
2024-07-12 00:14:46 +0300

    changes

--- src/pages/TimeTable.js
+++ src/pages/TimeTable.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import '../styles/App.css';
 import '../styles/OpacityPrewie.css'
+import TextParser from '../components/TimetableParser'
 function TimeTable() {
   return (
     <div>
       <h1 style={{ fontFamily: 'MyCustomFont', fontSize: '40px' }}>Расписание</h1>
       <select>
         <option value={'9IS>'}>
+          9-ИС123
+        </option>
+        <option value={'9IS>'}>
+          9-ИС321
+        </option>
+        <option value={'9IS>'}>
           9-ИС
         </option>
 
-
       </select>
-
+      <TextParser class = 'timetable'/>
     </div>
   );
 }
