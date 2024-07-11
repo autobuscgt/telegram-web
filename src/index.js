68900bd58135abbfeeac33db9cdd89744b908026
autobuscgt <lekiaaaskya@gmail.com>
2024-07-11 17:30:44 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import TimeTable from './pages/TimeTable';
+import Homework from './pages/Homework';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App/>
+    <Main/>
+    <TimeTable/>
+    <News/>
+    <Homework/>
   </React.StrictMode>
 );
 
