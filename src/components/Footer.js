eb108a46335f830078444801095e44bc84cc87b7
autobuscgt <lekiaaaskya@gmail.com>
2024-07-11 21:18:50 +0300

    changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,15 @@
 import React from "react";
 import '../styles/App.css';
 import logo from "../images/logotype.png";
+import '../styles/Footer.css'
 function Footer() {
     return (
         <div>
-            <footer style={{ margin: '45px 45px 45px 45px' }}>
-                <img src={logo} alt='logotype' class='logo' style={{ paddingBottom: 15, float: 'right' }}></img>
+            <footer>
+                <img src={logo} alt='logotype' class='logo' style={{  float: 'right' }}></img>
+                <a href='https://github.com/autobuscgt' style={{ fontFamily: 'MyCustomFont', fontSize: '32px' ,margin:'5px'}} >
+                    Создатель Autobus
+                </a>
             </footer>
         </div>
     );
