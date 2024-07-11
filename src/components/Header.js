0b23b2ce0a9a9a8e8ae632b33862625925dc8a82
autobuscgt <lekiaaaskya@gmail.com>
2024-07-11 21:51:07 +0300

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header(){
     return(
         <div>
         <header>
-        <h1 style={{ fontFamily: 'MyCustomFont'}} class='nameof'>MKIT WEB APP</h1>
+        <h1 style={{ fontFamily: 'MyCustomFont'}} class='nameof'>MKIT WEB APP O_o</h1>
         </header>
         </div>
     );
